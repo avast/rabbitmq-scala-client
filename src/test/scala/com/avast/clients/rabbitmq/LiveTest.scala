30cdabea4eead3c60d76e26e6a7ee0985f4807d7
kolena <kolena@avast.com>
2016-05-18 20:40:39 +0200

    Configuration improvement - multiple bindings

--- src/test/scala/com/avast/clients/rabbitmq/LiveTest.scala
+++ src/test/scala/com/avast/clients/rabbitmq/LiveTest.scala
@@ -4,11 +4,13 @@ import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.{CountDownLatch, Executors, TimeUnit}
 
 import com.avast.metrics.test.NoOpMonitor
-import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
+import com.typesafe.config.{Config, ConfigFactory, ConfigValueFactory}
+import net.ceedubs.ficus.Ficus._
 import org.scalatest.FunSuite
 import org.scalatest.concurrent.Eventually
 import org.scalatest.time.{Seconds, Span}
 
+import scala.collection.JavaConverters._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
 
@@ -16,12 +18,21 @@ class LiveTest extends FunSuite with Eventually {
   private def createConfig() = new {
 
     val queueName = randomString(10)
-    val exchange = randomString(10)
+    val exchange1 = randomString(10)
+    val exchange2 = randomString(10)
 
-    val config = ConfigFactory.load().getConfig("myConfig")
+    private val original = ConfigFactory.load().getConfig("myConfig")
+
+    val bindConfigs = original.as[Array[Config]]("consumer.binds")
+    bindConfigs(0) = bindConfigs(0).withValue("exchange.name", ConfigValueFactory.fromAnyRef(exchange1))
+    bindConfigs(1) = bindConfigs(1).withValue("exchange.name", ConfigValueFactory.fromAnyRef(exchange2))
+
+
+    val config = original
       .withValue("consumer.queueName", ConfigValueFactory.fromAnyRef(queueName))
-      .withValue("consumer.bind.exchange.name", ConfigValueFactory.fromAnyRef(exchange))
-      .withValue("producer.exchange", ConfigValueFactory.fromAnyRef(exchange))
+      .withValue("consumer.binds", ConfigValueFactory.fromIterable(bindConfigs.toSeq.map(_.root()).asJava))
+      .withValue("producer.exchange", ConfigValueFactory.fromAnyRef(exchange1))
+      .withValue("producer2.exchange", ConfigValueFactory.fromAnyRef(exchange2))
   }
 
   def randomString(length: Int): String = {
@@ -89,5 +100,34 @@ class LiveTest extends FunSuite with Eventually {
       assertResult(true)(latch.await(1000, TimeUnit.MILLISECONDS))
       assertResult(0)(testHelper.getMessagesCount(queueName))
     }
+  }
+
+  test("multiple producers to single consumer") {
+    val c = createConfig()
+    import c._
+
+    implicit val ex = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
+
+    val latch = new CountDownLatch(20)
+
+    val channelFactory = RabbitMQChannelFactory.fromConfig(config, Some(ex))
+
+    RabbitMQClientFactory.Consumer.fromConfig(config.getConfig("consumer"), channelFactory, NoOpMonitor.INSTANCE) { delivery =>
+      latch.countDown()
+      Future.successful(true)
+    }
+
+    val sender1 = RabbitMQClientFactory.Producer.fromConfig(config.getConfig("producer"), channelFactory, NoOpMonitor.INSTANCE)
+
+    val sender2 = RabbitMQClientFactory.Producer.fromConfig(config.getConfig("producer2"), channelFactory, NoOpMonitor.INSTANCE)
+
+    for (_ <- 1 to 10) {
+      sender1.send("test", Random.nextString(10).getBytes())
+      sender2.send("test2", Random.nextString(10).getBytes())
+    }
+
+    assertResult(true)(latch.await(500, TimeUnit.MILLISECONDS))
+
+    assertResult(0)(testHelper.getMessagesCount(queueName))
   }
 }
