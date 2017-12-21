9b41c85daa6b298b39dd50f315feaefa43b90200
kolena <kolena@avast.com>
2017-12-21 14:08:11 +0100

    close methods for consumer/producer in Java API

--- api/src/main/scala/com/avast/clients/rabbitmq/javaapi/RabbitMQConsumer.scala
+++ api/src/main/scala/com/avast/clients/rabbitmq/javaapi/RabbitMQConsumer.scala
@@ -5,7 +5,7 @@ import com.avast.clients.rabbitmq.api.{RabbitMQConsumer => ScalaConsumer}
 import scala.util.control.NonFatal
 import scala.util.{Failure, Success}
 
-class RabbitMQConsumer(scalaConsumer: ScalaConsumer) {
+class RabbitMQConsumer(scalaConsumer: ScalaConsumer) extends AutoCloseable {
   @throws[Exception]
   def bindTo(exchange: String, routingKey: String): Unit = {
     scalaConsumer.bindTo(exchange, routingKey) match {
@@ -13,4 +13,6 @@ class RabbitMQConsumer(scalaConsumer: ScalaConsumer) {
       case Failure(NonFatal(e)) => throw e // thrown intentionally, it's Java API!
     }
   }
+
+  override def close(): Unit = scalaConsumer.close()
 }
