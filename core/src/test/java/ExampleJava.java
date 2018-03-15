1d53ecd7d54d95ada5a1be44fe265ff8cb7e9a74
kolena <kolena@avast.com>
2018-03-15 17:18:57 +0100

    ExampleJava generalized

--- core/src/test/java/ExampleJava.java
+++ core/src/test/java/ExampleJava.java
@@ -4,32 +4,32 @@ import com.avast.metrics.test.NoOpMonitor;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
-import java.io.File;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 
 public class ExampleJava {
     public static void main(String[] args) {
-        File file = new File("/home/jenda/dev/rabbitmqclient/localhost.conf").getAbsoluteFile();
-        Config config = ConfigFactory.parseFile(file).getConfig("myConfig");
-        String routingKey = config.getString("consumer.queueName");
-
+        final Config config = ConfigFactory.load().getConfig("myConfig");
+        final String routingKey = config.getString("consumer.queueName");
 
         final ExecutorService executor = Executors.newCachedThreadPool();
+        final ForkJoinPool callbackExecutor = new ForkJoinPool();
 
         final RabbitMQJavaConnection connection = RabbitMQJavaConnection.newBuilder(config, executor).build();
 
         final RabbitMQConsumer rabbitMQConsumer = connection.newConsumer(
                 "consumer",
                 NoOpMonitor.INSTANCE,
-                executor,
+                callbackExecutor,
                 ExampleJava::handleDelivery
         );
 
-        final RabbitMQProducer rabbitMQProducer = connection.newProducer("producer",
+        final RabbitMQProducer rabbitMQProducer = connection.newProducer(
+                "producer",
                 NoOpMonitor.INSTANCE,
-                executor
+                callbackExecutor
         );
 
         for (int i = 0; i < 1000; i++) {
@@ -41,7 +41,7 @@ public class ExampleJava {
         }
     }
 
-    public static CompletableFuture<DeliveryResult> handleDelivery(Delivery delivery) {
+    private static CompletableFuture<DeliveryResult> handleDelivery(Delivery delivery) {
         System.out.println(delivery.getBody().toStringUtf8());
         return CompletableFuture.completedFuture(DeliveryResult.Ack());
     }
