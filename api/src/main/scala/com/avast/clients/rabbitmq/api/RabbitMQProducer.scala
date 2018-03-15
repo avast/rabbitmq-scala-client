f29eafa65a727eda6a4e7727c97353a47b862287
kolena <kolena@avast.com>
2018-03-15 16:19:17 +0100

    Put back autoFunctorK generation for RabbitMQProducer

--- api/src/main/scala/com/avast/clients/rabbitmq/api/RabbitMQProducer.scala
+++ api/src/main/scala/com/avast/clients/rabbitmq/api/RabbitMQProducer.scala
@@ -1,9 +1,11 @@
 package com.avast.clients.rabbitmq.api
 
 import com.avast.bytes.Bytes
+import mainecoon.autoFunctorK
 
 import scala.language.higherKinds
 
+@autoFunctorK(autoDerivation = false)
 trait RabbitMQProducer[F[_]] {
   def send(routingKey: String, body: Bytes, properties: Option[MessageProperties] = None): F[Unit]
 }
--- api/src/main/scala/com/avast/clients/rabbitmq/javaapi/RabbitMQProducer.scala
+++ api/src/main/scala/com/avast/clients/rabbitmq/javaapi/RabbitMQProducer.scala
@@ -4,9 +4,6 @@ import java.util.concurrent.CompletableFuture
 
 import com.avast.bytes.Bytes
 
-import scala.collection.JavaConverters._
-import scala.language.implicitConversions
-
 trait RabbitMQProducer extends AutoCloseable {
   @throws[Exception]
   def send(routingKey: String, body: Bytes): CompletableFuture[Void]
