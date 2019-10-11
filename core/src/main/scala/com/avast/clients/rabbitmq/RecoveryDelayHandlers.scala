4d245aab20bd130a7d0c7b81b579dc792c77e247
kolena <kolena@avast.com>
2019-10-11 15:45:26 +0200

    Defaults to case classes from deleted reference.conf

--- core/src/main/scala/com/avast/clients/rabbitmq/RecoveryDelayHandlers.scala
+++ core/src/main/scala/com/avast/clients/rabbitmq/RecoveryDelayHandlers.scala
@@ -1,16 +1,18 @@
 package com.avast.clients.rabbitmq
-import java.time.Duration
 
 import com.rabbitmq.client.RecoveryDelayHandler
 
-private[rabbitmq] object RecoveryDelayHandlers {
-  case class Linear(delay: Duration, period: Duration) extends RecoveryDelayHandler {
+import scala.concurrent.duration._
+
+object RecoveryDelayHandlers {
+  case class Linear(delay: Duration = 1.second, period: Duration = 5.seconds) extends RecoveryDelayHandler {
     override def getDelay(recoveryAttempts: Int): Long = {
       if (recoveryAttempts == 0) delay.toMillis else period.toMillis
     }
   }
 
-  case class Exponential(delay: Duration, period: Duration, factor: Double, maxLength: Duration) extends RecoveryDelayHandler {
+  case class Exponential(delay: Duration = 1.second, period: Duration = 5.seconds, factor: Double = 2.0, maxLength: Duration = 32.seconds)
+      extends RecoveryDelayHandler {
     private val maxMillis = maxLength.toMillis
 
     override def getDelay(recoveryAttempts: Int): Long = {
