ddf998754b470667b7aa7b5135dc447c8b123111
kolena <kolena@avast.com>
2019-11-19 10:35:39 +0100

    Adjusting recovery delays according to recommendations in official docs

--- core/src/main/scala/com/avast/clients/rabbitmq/RecoveryDelayHandlers.scala
+++ core/src/main/scala/com/avast/clients/rabbitmq/RecoveryDelayHandlers.scala
@@ -4,14 +4,16 @@ import com.rabbitmq.client.RecoveryDelayHandler
 
 import scala.concurrent.duration._
 
+// Delay intervals should avoid values that are too low (lower than 2 seconds).
+// https://www.rabbitmq.com/api-guide.html#automatic-recovery-limitations
 object RecoveryDelayHandlers {
-  case class Linear(initialDelay: Duration = 1.second, period: Duration = 5.seconds) extends RecoveryDelayHandler {
+  case class Linear(initialDelay: Duration = 5.second, period: Duration = 5.seconds) extends RecoveryDelayHandler {
     override def getDelay(recoveryAttempts: Int): Long = {
       if (recoveryAttempts == 0) initialDelay.toMillis else period.toMillis
     }
   }
 
-  case class Exponential(initialDelay: Duration = 1.second,
+  case class Exponential(initialDelay: Duration = 5.second,
                          period: Duration = 5.seconds,
                          factor: Double = 2.0,
                          maxLength: Duration = 32.seconds)
