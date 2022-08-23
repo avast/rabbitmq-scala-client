d8118214af33bbb98972eca7f6a66d1055fb361c
Karel Fajkus <karel.fajkus@avast.com>
2022-08-23 15:21:11 +0200

    PR fix

--- core/src/main/scala/com/avast/clients/rabbitmq/ExponentialDelay.scala
+++ core/src/main/scala/com/avast/clients/rabbitmq/ExponentialDelay.scala
@@ -6,12 +6,12 @@ import scala.concurrent.duration.{Duration, FiniteDuration}
 class ExponentialDelay(initialDelay: Duration, period: Duration, factor: Double, maxLength: Duration) {
   private val maxMillis = maxLength.toMillis
 
-  def getExponentialDelay(attempts: Int): FiniteDuration = {
-    if (attempts == 0) FiniteDuration(initialDelay._1, initialDelay._2)
+  def getExponentialDelay(attempt: Int): FiniteDuration = {
+    if (attempt == 0) FiniteDuration(initialDelay._1, initialDelay._2)
     else {
       val millis = math.min(
         maxMillis,
-        (period.toMillis * math.pow(factor, attempts - 1)).toLong
+        (period.toMillis * math.pow(factor, attempt - 1)).toLong
         )
       FiniteDuration(millis, TimeUnit.MILLISECONDS)
     }
